b9020de27c3836dd1ea0690a995a8b907d4f4424
Md Noble <mdnoble.wd@gmail.com>
2024-01-11 19:54:42 +0600

    added style in task card

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,13 +1,18 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-const TaskCard = ({task, 
-    tasks, 
-    setTasks}) => {
-    return (
-        <div>
-            <h2>{task.name}</h2>
-        </div>
-    );
+import { CiCircleRemove } from "react-icons/ci";
+
+const TaskCard = ({ task, tasks, setTasks }) => {
+  return (
+    <section className="my-2">
+      <div className="flex items-center justify-between cursor-grab rounded-md drop-shadow-2xl bg-slate-100 px-4 py-3">
+        <h2 className="font-bold text-xl text-black capitalize">{task.name}</h2>
+        <button className="font-bold text-3xl text-red-600">
+          <CiCircleRemove></CiCircleRemove>
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default TaskCard;
