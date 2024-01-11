38d1557ebf3c58fb4aca92b327835a59826daa55
Md Noble <mdnoble.wd@gmail.com>
2024-01-11 13:03:44 +0600

    added a text input area and button on CreateTask component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from "react"
+import { useState } from "react";
 import CreateTask from "./components/CreateTask";
 import ListTasks from "./components/ListTasks";
 
 function App() {
-
-  const [ tasks, setTasks ] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
   return (
-    <section>
-      <h2 className="text-6xl">React DND TODO App</h2>
+    <section className="max-w-screen-2xl mx-auto bg-slate-200 w-full h-screen flex flex-col gap-10 items-center py-16">
       <CreateTask tasks={tasks} setTasks={setTasks}></CreateTask>
       <ListTasks tasks={tasks} setTasks={setTasks}></ListTasks>
     </section>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/CreateTask.jsx
+++ src/components/CreateTask.jsx
@@ -1,7 +1,10 @@
 const CreateTask = ( { tasks, setTasks } ) => {
     return (
         <div>
-           <h2>Create Task</h2> 
+           <form>
+            <input type="text" name="taskName" className="border-2 border-slate-400 bg-slate-100 rounded-md h-12 w-72 mr-4 p-4"/>
+            <button className="bg-sky-400 px-4 py-2 rounded-md font-bold text-lg uppercase">Create</button>
+           </form>
         </div>
     );
 };
