058759681b106517bed27ac28978abe2b81806db
Md Noble <mdnoble.wd@gmail.com>
2024-01-11 14:58:19 +0600

    filtering tasks to show on task list

--- src/components/ListTasks.jsx
+++ src/components/ListTasks.jsx
@@ -1,7 +1,26 @@
-const ListTasks = ( { tasks, setTasks } ) => {
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+
+const ListTasks = ({tasks, setTasks }) => {
 
 
-    
+    const [ todoTasks, setTodoTasks ] = useState([]);
+    const [ ongoingTasks, setOngoingTasks ] = useState([]);
+    const [ completedTasks, setCompletedTasks ] = useState([]);
+
+    useEffect( () => {
+
+        const fTodo = tasks.filter(task => task.status === "todo");
+        const fOngoing = tasks.filter(task => task.status === "ongoing");
+        const fCompleted = tasks.filter(task => task.status === "completed");
+
+        setTodoTasks(fTodo);
+        setOngoingTasks(fOngoing);
+        setCompletedTasks(fCompleted);
+
+
+    }, [tasks])
 
 
 
