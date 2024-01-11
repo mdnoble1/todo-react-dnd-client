adc1dfd17ad58707fc3ca0d689801f0a97de5149
Md Noble <mdnoble.wd@gmail.com>
2024-01-11 13:13:41 +0600

    installed uuid4 to generate random id for tasks

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   return (
-    <section className="max-w-screen-2xl mx-auto bg-slate-200 w-full h-screen flex flex-col gap-10 items-center py-16">
+    <section className="max-w-screen-2xl mx-auto bg-slate-200 w-full h-screen flex flex-col gap-10 items-center py-20">
       <CreateTask tasks={tasks} setTasks={setTasks}></CreateTask>
       <ListTasks tasks={tasks} setTasks={setTasks}></ListTasks>
     </section>
