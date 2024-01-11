7ce4201b4542cce0e6d1d87ce1e3591c2e1d8122
Md Noble <mdnoble.wd@gmail.com>
2024-01-11 13:14:01 +0600

    installed uuid4 to generate random id for tasks

--- src/components/CreateTask.jsx
+++ src/components/CreateTask.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 const CreateTask = ( { tasks, setTasks } ) => {
+
+
+    // assigning task from the input field 
+    const [ task, setTask ] = useState({
+        id: "",
+        name: "",
+        status: "todo"  // can also be ongoing and completed 
+    })
+
+
     return (
         <div>
            <form>
-            <input type="text" name="taskName" className="border-2 border-slate-400 bg-slate-100 rounded-md h-12 w-72 mr-4 p-4"/>
+            <input type="text" name="taskName" className="border-2 border-slate-400 bg-slate-100 rounded-md h-12 w-72 mr-4 p-4"
+            onChange={ (e) => setTask( {...task, id: }) }/>
             <button className="bg-sky-400 px-4 py-2 rounded-md font-bold text-lg uppercase">Create</button>
            </form>
         </div>
