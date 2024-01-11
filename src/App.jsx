6fd296789efb655626494ad75cc61b5131d46eb2
Md Noble <mdnoble.wd@gmail.com>
2024-01-11 12:46:52 +0600

    created CreateTask component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css'
+import { useState } from "react"
+import CreateTask from "./components/CreateTask";
 
 function App() {
 
+  const [ tasks, setTasks ] = useState([]);
+
   return (
-    <>
-      <h2>React DND TODO App</h2>
-    </>
+    <section>
+      <h2 className="text-6xl">React DND TODO App</h2>
+      <CreateTask tasks={tasks} setTasks={setTasks}></CreateTask>
+    </section>
   )
 }
 
---
+++ src/components/CreateTask.jsx
@@ -0,0 +1,9 @@
+const CreateTask = ( { tasks, setTasks } ) => {
+    return (
+        <div>
+           <h2>Create Task</h2> 
+        </div>
+    );
+};
+
+export default CreateTask;
