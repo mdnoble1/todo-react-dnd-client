44a2702b293c887b7296df80b98ec477855af049
Md Noble <mdnoble.wd@gmail.com>
2024-01-11 13:25:52 +0600

    created a form to collect task data from input field

--- src/components/CreateTask.jsx
+++ src/components/CreateTask.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 const CreateTask = ( { tasks, setTasks } ) => {
 
@@ -8,14 +9,17 @@ const CreateTask = ( { tasks, setTasks } ) => {
         id: "",
         name: "",
         status: "todo"  // can also be ongoing and completed 
-    })
+    });
+
+    console.log(task);
 
 
     return (
         <div>
            <form>
             <input type="text" name="taskName" className="border-2 border-slate-400 bg-slate-100 rounded-md h-12 w-72 mr-4 p-4"
-            onChange={ (e) => setTask( {...task, id: }) }/>
+            value={task.name}
+            onChange={ (e) => setTask( {...task, id: uuidv4(), name: e.target.value }) }/>
             <button className="bg-sky-400 px-4 py-2 rounded-md font-bold text-lg uppercase">Create</button>
            </form>
         </div>
